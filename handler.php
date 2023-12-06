fe54cbb71381b1b7120abe64c7e87c237f8cbde8
viraj-thummar <110028089+viraj-thummar@users.noreply.github.com>
2023-12-06 11:40:29 +0530

    form

--- handler.php
+++ handler.php
@@ -8,8 +8,8 @@ require 'php/PHPMailer-5.2.28/src/PHPMailer.php';
 require 'php/PHPMailer-5.2.28/src/SMTP.php';
 
 $mail = new PHPMailer(true);
-$mail_to_email = '***@gmail.com'; // your email
-$mail_to_name = 'Webmaster';
+$mail_to_email = 'virajthummar2@gmail.com'; // your email
+$mail_to_name = 'Viraj';
 
 try {
 
@@ -20,12 +20,12 @@ try {
 
 	// Server settings
 	$mail->isSMTP(); // Send using SMTP
-	$mail->Host = 'smtp.***.com'; // Set the SMTP server to send through
+	$mail->Host = 'smtp.elasticemail.com'; // Set the SMTP server to send through
 	$mail->SMTPAuth = true; // Enable SMTP authentication
-	$mail->Username = '***'; // SMTP username
-	$mail->Password = '***'; // SMTP password
+	$mail->Username = 'virajthummar2@gmail.com'; // SMTP username
+	$mail->Password = '9DE47AFD1F0428ABB2DD9BDEC32FDF144F4D'; // SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+	$mail->Port = 2525; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 	$mail->setFrom($mail_to_email, $mail_to_name); // Your email
 	$mail->addAddress($mail_from_email, $mail_from_name); // Add a recipient
